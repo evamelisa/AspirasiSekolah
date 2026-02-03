a45c7703d1abb24b963c0bfa11cce036ef1a8de5
evamelisa-rpl <evamelisa2007@gmail.com>
2026-02-03 10:19:04 +0700

    chore: add comment to clarify AuthController functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
-
+//handle auth
 class AuthController extends Controller
 {   
     public function showLoginForm() {
