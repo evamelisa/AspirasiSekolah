8e137ca40684b4aa53d54ea00ba02cf270d3561f
evamelisa-rpl <evamelisa2007@gmail.com>
2026-02-03 08:55:06 +0700

    refactor : move code showRegister to UserController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Hash;
 
 
 class AuthController extends Controller
-{
+{   
     public function showLoginForm() {
         $title = 'Login';
         return view('auth.login', compact('title'));
@@ -25,32 +25,6 @@ class AuthController extends Controller
         }
 
         return back()->withErrors(['username' => 'Username atau password salah']);
-    }
-
-    public function showRegisterForm() {
-        $title = 'Daftar User Baru';
-        return view('auth.register', compact('title'));
-    }
-
-    public function register(Request $request) {
-        $request->validate([
-            'name' => 'required',
-            'username' => 'required|unique:users,username|max:255',
-            'nis' => 'required|unique:users,nis|max:20',
-            'password' => 'required|min:6',
-
-        ]);
-
-        $user = User::create([
-            'name' => $request->name,
-            'username' => $request->username,
-            'nis' => $request->nis,
-            'password' => Hash::make($request->password),
-            'role' => 'user',
-        ]);
-
-        //Redirect ke halaman aspirasi dengan pesan sukses
-        return redirect()->route('aspirasi.index')->with('success', 'User"'. $user->name .'" berhasil didaftarkan.');
     }
 
     public function logout(Request $request) {
