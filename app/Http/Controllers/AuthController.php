1ffe1f4b98471c0099e659d6639159f914a1183f
evamelisa-rpl <evamelisa2007@gmail.com>
2026-02-10 07:53:41 +0700

    fix: format login method declaration in AuthController for consistency

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
         return view('auth.login', compact('title'));
     }
 
-    public function login(Request $request) {
+    public function login(Request $request) {   
         $credentials = $request->only('username', 'password');
 
         if (Auth::attempt($credentials)) {
